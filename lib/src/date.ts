c538feb6525c934871cc0cd312de07b3c503c3b4
Alexey Kryvets <alex.kryvets@gmail.com>
2021-08-14 13:52:04 +0300

    dev

--- lib/src/date.ts
+++ lib/src/date.ts
@@ -4,8 +4,7 @@ export function dateFieldsToString(input: any, format: string, fields: any, recu
             if (input.hasOwnProperty(key) && key.indexOf('$') !== 0) {
                 if (input[key] instanceof Date && (!fields || fields.indexOf(key) !== -1)) {
                     input[key] = input[key].toString(format);
-                }
-                else if (recursive && typeof input[key] === 'object') {
+                } else if (recursive && typeof input[key] === 'object') {
                     dateFieldsToString(input[key], format, fields);
                 }
             }
@@ -16,6 +15,17 @@ export function dateFieldsToString(input: any, format: string, fields: any, recu
 const regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})([\-+])(\d{2}):?(\d{2}))?)?)?$/;
 const html5Format = /^(\d{4})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2})(?::(\d{2}))?$/;
 const dateFormat = /^(\d{4})-(\d{2})-(\d{2})$/;
+
+export function dateParse(date: string): number {
+    const timestamp = Date.parse(date);
+    if (!isNaN(timestamp)) {
+        return timestamp;
+    } else {
+        return Date.parse(
+            date.replace(/-/g, '/').replace(/[a-z]+/gi, ' ')
+        );
+    }
+}
 
 export function convertStringToDate(input: any): any {
     if (typeof input !== 'object') {
@@ -29,19 +39,17 @@ export function convertStringToDate(input: any): any {
         let match;
         // Date only in full format "match[9]"
         if (typeof value === 'string' && ((match = value.match(regexIso8601)) && match[9] || (match = value.match(html5Format)) && match[6])) {
-            const milliseconds = Date.parse(match[0]);
+            const milliseconds = dateParse(match[0]);
             if (!isNaN(milliseconds)) {
                 input[key] = new Date(milliseconds);
             }
-        }
-        else if (typeof value === 'string' && (match = value.match(dateFormat)) && match[3]) {
-            const milliseconds = Date.parse(match[0]);
+        } else if (typeof value === 'string' && (match = value.match(dateFormat)) && match[3]) {
+            const milliseconds = dateParse(match[0]);
             if (!isNaN(milliseconds)) {
                 const date = new Date(milliseconds);
                 input[key] = date;
             }
-        }
-        else if (typeof value === 'object') {
+        } else if (typeof value === 'object') {
             convertStringToDate(value);
         }
     }
